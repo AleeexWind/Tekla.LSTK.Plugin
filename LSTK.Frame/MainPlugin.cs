b22c0ba1c51341cb7fae95b8720a0aaa5c1a22a1
AleeexWind <alex_wind@mail.ru>
2023-05-24 23:15:38 +0300

    Add project files.

---
+++ LSTK.Frame/MainPlugin.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tekla.Structures.Model;
+using Tekla.Structures.Plugins;
+
+namespace LSTK.Frame
+{
+    [Plugin("LSTK_Frame")]
+    [PluginUserInterface("LSTK.Frame.MainWindow")]
+    public class MainPlugin : PluginBase
+    {
+        private Model _Model;
+        private PluginData _Data;
+
+        private string _profile = string.Empty;
+        private Model Model
+        {
+            get { return this._Model; }
+            set { this._Model = value; }
+        }
+
+        private PluginData Data
+        {
+            get { return this._Data; }
+            set { this._Data = value; }
+        }
+
+        public MainPlugin(PluginData data)
+        {
+            Model = new Model();
+            Data = data;
+        }
+
+
+        public override List<InputDefinition> DefineInput()
+        {
+            return new List<InputDefinition>();
+        }
+
+        public override bool Run(List<InputDefinition> Input)
+        {
+            return true;
+        }
+    }
+    public class PluginData
+    {
+        #region Fields
+        //
+        // Define the fields specified on the Form.
+        //
+        [StructuresField("name")]
+        public string partName;
+         
+        [StructuresField("profile")]
+        public string profile;
+
+        #endregion
+    }
+}
---
+++ LSTK.Frame/MainWindowViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Tekla.Structures.Dialog;
+using TD = Tekla.Structures.Datatype;
+
+namespace LSTK.Frame
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string partName = string.Empty;
+        private string profile = string.Empty;
+
+        [StructuresDialog("name", typeof(TD.String))]
+        public string PartName
+        {
+            get { return partName; }
+            set { partName = value; OnPropertyChanged("PartName"); }
+        }
+        [StructuresDialog("profile", typeof(TD.String))]
+        public string ProfileName
+        {
+            get { return profile; }
+            set { profile = value; OnPropertyChanged("ProfileName"); }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        public void Run()
+        {
+            MessageBox.Show($"Start. Part: {partName}  Profile: {profile}");
+        }
+    }
+}
