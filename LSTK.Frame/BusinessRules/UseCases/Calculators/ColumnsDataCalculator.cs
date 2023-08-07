e2bf178eca90d35ea40ba09e0e1745dc1ddaa41c
AleeexWind <alex_wind@mail.ru>
2023-08-07 12:45:42 +0300

    Implement columnsData calculating

--- LSTK.Frame/BusinessRules/UseCases/Calculators/ColumnsDataCalculator.cs
+++ LSTK.Frame/BusinessRules/UseCases/Calculators/ColumnsDataCalculator.cs
@@ -11,13 +11,63 @@ namespace LSTK.Frame.BusinessRules.UseCases.Calculators
     public class ColumnsDataCalculator
     {
         private FrameData _frameData;
+        private FrameInputData _frameInputData;
         public ColumnsDataCalculator(FrameData frameData)
         {
             _frameData = frameData;
         }
         public void Calculate(FrameInputData frameInputData)
         {
-            _frameData.ColumnsData = new ColumnsData();
+            _frameInputData = frameInputData;
+            ColumnsData columnsData = new ColumnsData()
+            {
+                LeftColumn = CalcLeftColumn(),
+                RightColumn = CalcRightColumn()
+            };
+
+            _frameData.ColumnsData = columnsData;
+        }
+        private ElementData CalcLeftColumn()
+        {
+            Point startPoint = new Point()
+            {
+                X = 0.0,
+                Y = 0.0,
+                Z = 0.0
+            };
+            Point endPoint = new Point()
+            {
+                X = 0.0,
+                Y = _frameInputData.HeightColumns,
+                Z = 0.0
+            };
+            ElementData elementData = new ElementData()
+            {
+                StartPoint = startPoint,
+                EndPoint = endPoint
+            };
+            return elementData;
+        }
+        private ElementData CalcRightColumn()
+        {
+            Point startPoint = new Point()
+            {
+                X = _frameInputData.Bay,
+                Y = 0.0,
+                Z = 0.0
+            };
+            Point endPoint = new Point()
+            {
+                X = _frameInputData.Bay,
+                Y = _frameInputData.HeightColumns,
+                Z = 0.0
+            };
+            ElementData elementData = new ElementData()
+            {
+                StartPoint = startPoint,
+                EndPoint = endPoint
+            };
+            return elementData;
         }
     }
 }
