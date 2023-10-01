78aa1a65469c96e3261235a81390a3708a707bb2
AleeexWind <alex_wind@mail.ru>
2023-10-01 13:20:01 +0300

    Implement TrussPostsRight creation without Y coordinate

--- LSTK.Frame/BusinessRules/UseCases/Calculators/SchemaCalculators/TrussPostsSchemaCalculator.cs
+++ LSTK.Frame/BusinessRules/UseCases/Calculators/SchemaCalculators/TrussPostsSchemaCalculator.cs
@@ -9,23 +9,75 @@ namespace LSTK.Frame.BusinessRules.UseCases.Calculators.SchemaCalculators
     {
         private SchemaInputData _schemaInputData;
         private FrameData _frameData;
-        private List<double> _trussPostDistances;
+        private string[] _distances;
+        private List<double> _trussPostDistancesLeft;
+        double _previousCoord = 0;
+        //private List<double> _trussPostDistancesLeft;
+        //private List<double> _trussPostDistancesRight;
         public void Calculate(FrameData frameData, InputData inputData)
         {
-            _trussPostDistances = new List<double>();
             _schemaInputData = inputData as SchemaInputData;
             _frameData = frameData;
-            ParsePanelsString();
-            _frameData.TrussData.TrussPosts = CalcTrussPosts();
+            _distances = ParsePanelsString();
+            _previousCoord = 0;
+
+            List<ElementData> trussPostsElements = new List<ElementData>();
+            trussPostsElements.AddRange(CalcLeftTrussPosts());
+            trussPostsElements.AddRange(CalcRightThrussPosts());
+
+            _frameData.TrussData.TrussPosts = trussPostsElements;
         }
-        private void ParsePanelsString()
+        private string[] ParsePanelsString()
         {
             string[] p = _schemaInputData.Panels.Split(' ');
-            for (int i = 0; i < p.Length; i++)
-            {
-                _trussPostDistances.AddRange(GetDistances(p[i]));
-            }
+            return p;
         }
+        //private void ParsePanelsString()
+        //{
+        //    string[] p = _schemaInputData.Panels.Split(' ');
+        //    for (int i = 0; i < p.Length; i++)
+        //    {
+        //        _trussPostDistancesRight.AddRange(GetDistances(p[i]));
+        //    }
+        //}
+        private List<double> GetLeftDistances(string[] distancesAsString)
+        {
+            List<double> distances = new List<double>();
+            for (int i = 0; i < distancesAsString.Length; i++)
+            {
+                string[] p = distancesAsString[i].Split('*');
+
+                double amount = 0;
+                double dist = 0;
+
+                if (p.Length > 1)
+                {
+                    amount = double.Parse(p[0], System.Globalization.CultureInfo.InvariantCulture);
+                    dist = double.Parse(p[1], System.Globalization.CultureInfo.InvariantCulture);
+                }
+                else
+                {
+                    amount = 1;
+                    dist = double.Parse(p[0], System.Globalization.CultureInfo.InvariantCulture);
+                }
+
+                for (int r = 0; r < amount; r++)
+                {
+                    distances.Add(dist);
+                }
+            }
+            return distances;
+        }
+        private List<double> GetRightDistances(List<double> leftDistances)
+        {
+            double startOffsetX = (_schemaInputData.Bay/2 - _previousCoord) * 2;
+            List<double> distances = leftDistances;
+            distances.Add(startOffsetX);
+            distances = distances.Reverse<double>().ToList();
+            return distances;
+        }
+
+
         private List<double> GetDistances(string value)
         {
             List<double> distances = new List<double>();
@@ -51,19 +103,20 @@ namespace LSTK.Frame.BusinessRules.UseCases.Calculators.SchemaCalculators
             }
             return distances;
         }
-        private List<ElementData> CalcTrussPosts()
+        private List<ElementData> CalcLeftTrussPosts()
         {
             List<ElementData> result = new List<ElementData>();
-            if (_trussPostDistances.Any())
+            _trussPostDistancesLeft = GetLeftDistances(_distances);
+            if (_trussPostDistancesLeft.Any())
             {
-                _trussPostDistances.RemoveAt(_trussPostDistances.Count -1);
-                double previousCoord = 0;
-                foreach (var dist in _trussPostDistances)
+                _trussPostDistancesLeft.RemoveAt(_trussPostDistancesLeft.Count -1);
+                //double _previousCoord = 0;
+                foreach (var dist in _trussPostDistancesLeft)
                 {
-                    previousCoord += dist;
+                    _previousCoord += dist;
                     Point startPoint = new Point()
                     {
-                        X = previousCoord,
+                        X = _previousCoord,
                         Y = _frameData.TrussData.RightBottomChord.StartPoint.Y,
                         Z = 0.0
                     };
@@ -71,13 +124,59 @@ namespace LSTK.Frame.BusinessRules.UseCases.Calculators.SchemaCalculators
                     double allLength = _frameData.TrussData.LeftTopChord.EndPoint.X - _frameData.TrussData.LeftTopChord.StartPoint.X;
                     double allHeigtht = _frameData.TrussData.LeftTopChord.EndPoint.Y - _frameData.TrussData.LeftTopChord.StartPoint.Y;
 
-                    double lengthFromZero = previousCoord -_frameData.TrussData.LeftTopChord.StartPoint.X;
+                    double lengthFromZero = _previousCoord -_frameData.TrussData.LeftTopChord.StartPoint.X;
                     double offsetY = _frameData.TrussData.RightBottomChord.StartPoint.Y + _schemaInputData.HeightRoofBottom;
                     double additionalOffset = _frameData.TrussData.LeftTopChord.StartPoint.Y - _schemaInputData.HeightColumns;
 
                     offsetY += additionalOffset;
 
-                    Point endPoint = GetTrussPostEndPointOnTheLine(lengthFromZero, allLength, allHeigtht, offsetY, previousCoord);
+                    Point endPoint = GetTrussPostEndPointOnTheLine(lengthFromZero, allLength, allHeigtht, offsetY, _previousCoord);
+
+                    ElementData elementData = new ElementData()
+                    {
+                        StartPoint = startPoint,
+                        EndPoint = endPoint
+                    };
+                    result.Add(elementData);
+                }
+            }
+
+            return result;
+        }
+
+        private List<ElementData> CalcRightThrussPosts()
+        {
+            List<ElementData> result = new List<ElementData>();
+            List<double> trussPostDistancesRight = GetRightDistances(_trussPostDistancesLeft);           
+            if (trussPostDistancesRight.Any())
+            {
+                trussPostDistancesRight.RemoveAt(trussPostDistancesRight.Count -1);
+                foreach (var dist in trussPostDistancesRight)
+                {
+                    _previousCoord += dist;
+                    Point startPoint = new Point()
+                    {
+                        X = _previousCoord,
+                        Y = _frameData.TrussData.RightBottomChord.StartPoint.Y,
+                        Z = 0.0
+                    };
+
+                    double allLength = _frameData.TrussData.RightTopChord.EndPoint.X - _frameData.TrussData.RightTopChord.StartPoint.X;
+                    double allHeigtht = _frameData.TrussData.RightTopChord.EndPoint.Y - _frameData.TrussData.RightTopChord.StartPoint.Y;
+
+                    double lengthFromZero = _previousCoord -_frameData.TrussData.LeftTopChord.StartPoint.X;
+                    double offsetY = _frameData.TrussData.RightBottomChord.StartPoint.Y + _schemaInputData.HeightRoofBottom;
+                    double additionalOffset = _frameData.TrussData.LeftTopChord.StartPoint.Y - _schemaInputData.HeightColumns;
+
+                    offsetY += additionalOffset;
+
+                    //Point endPoint = GetTrussPostEndPointOnTheLine(lengthFromZero, allLength, allHeigtht, offsetY, _previousCoord);
+                    Point endPoint = new Point()
+                    {
+                        X = startPoint.X,
+                        Y = startPoint.Y + 1000,
+                        Z = 0
+                    };
 
                     ElementData elementData = new ElementData()
                     {
