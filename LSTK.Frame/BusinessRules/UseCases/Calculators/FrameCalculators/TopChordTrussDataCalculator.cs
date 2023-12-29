d54f9cb1448405b08138a4c7d727eb4a54b78f8d
AleeexWind <alex_wind@mail.ru>
2023-12-29 20:34:46 +0300

    Fix TopChord coordinates

--- LSTK.Frame/BusinessRules/UseCases/Calculators/FrameCalculators/TopChordTrussDataCalculator.cs
+++ LSTK.Frame/BusinessRules/UseCases/Calculators/FrameCalculators/TopChordTrussDataCalculator.cs
@@ -76,15 +76,17 @@ namespace LSTK.Frame.BusinessRules.UseCases.Calculators.FrameCalculators
         {
             try
             {
+                double offset = _leftTopChord.EndPoint.X - _frameBuildInputData.Bay/2;
+
                 Point startPoint = new Point()
                 {
-                    X = _frameBuildInputData.Bay,
+                    X = _frameBuildInputData.Bay - offset,
                     Y = _leftTopChord.StartPoint.Y,
                     Z = 0.0
                 };
                 Point endPoint = new Point()
                 {
-                    X = _frameBuildInputData.Bay/2,
+                    X = _frameBuildInputData.Bay/2 - offset,
                     Y = _leftTopChord.EndPoint.Y,
                     Z = 0.0
                 };
